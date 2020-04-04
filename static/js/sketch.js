d0158809e6a0473f80c5ee0d7800fdb077255207
Adriane Replogle <Adriane@Adrianes-MacBook-Air.local>
2020-04-04 19:50:16 +0200

    ...

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -7,7 +7,7 @@ let amplitude = 100.0; // Height of wave
 let period = 1000.0; // How many pixels before the wave repeats
 let dx; // Value for incrementing x
 let yvalues; // Using an array to store height values for the wave
-let emotion = "angry";
+
 
 
 
@@ -15,7 +15,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   // Define colors
   c1 = color("white");
-  c2 = color("yellow"); //Color to change with emotion
+  c2 = color(bkg_color); //Color to change with emotion
 
   w = width + 16;
   dx = (TWO_PI / period) * xspacing;
@@ -32,16 +32,7 @@ function draw() {
   renderWave();
 }
 
-function setGradient(c1, c2) {
-  // noprotect
-  noFill();
-  for (var y = 0; y < height; y++) {
-    var inter = map(y, 0, height, 0, 1);
-    var c = lerpColor(c1, c2, inter);
-    stroke(c);
-    line(0, y, width, y);
-  }
-}
+
 
 function calcWave() {
   // Increment theta (try different values for
@@ -50,24 +41,36 @@ function calcWave() {
 
   // For every x value, calculate a y value with sine function
   let x = theta;
-  for (let i = 0; i < yvalues.length; i++) {g
+  for (let i = 0; i < yvalues.length; i++) {
     switch(emotion) {
-      case "happy":
+      case "happiness":
+      case "surprise":
         yvalues[i] = sin(x) * amplitude; // sine
+        //bkg_color = "yellow";
         break;
-      case "angry":
+
+
+      case "anger":
+      case "contempt":
+      case "disgust":
+      case "fear":
+      case "sadness":
         yvalues[i] = (x * amplitude) % 150; // saw
+        break;
+
+      case "neutral":
+        yvalues[i] = x; // line
         break;
       default:
         yvalues[i] = (x % 5) < 5/2 ? x*amplitude : 0; // square
         break;
     }
     x += dx;
-    if(x < 5) {
+    /*if(x < 5) {
       emotion = "happy";
     } else {
       emotion = "angry";
-    }
+    }*/
   }
 }
 
@@ -86,3 +89,18 @@ function renderWave() {
   vertex(width, height);
   endShape();
 }
+
+function setGradient(c1, c2) {
+  // noprotect
+  noFill();
+  for (var y = 0; y < height; y++) {
+    var inter = map(y, 0, height, 0, 1);
+    var c = lerpColor(c1, c2, inter);
+    stroke(c);
+    line(0, y, width, y);
+  }
+}
+
+//{ N 'anger': 0, 'contempt': 0, 'disgust': 0, 'fear': 0, 'sadness', ': 0,
+//         'neutral'
+//         'happiness', 'surprise'
